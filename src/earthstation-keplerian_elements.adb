6bcd12674d1a1ad687167ea1ff153463197538f8
John Vinters <john.vinters@googlemail.com>
2010-05-30 20:33:19 +0100

    Add EarthStation.Keplerian_Elements used to load and save satellite data from disc.

---
+++ src/earthstation-keplerian_elements.adb
@@ -0,0 +1,374 @@
+--
+--  Copyright (C) 2010 John Vinters
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License along
+--  with this program; if not, see <http://www.gnu.org/licenses/>.
+--
+--  earthstation-keplerian_elements.adb		jvinters	26-May-2010
+--
+
+pragma License (GPL);
+
+with Ada.Directories;			use Ada.Directories;
+with Ada.IO_Exceptions;
+with Ada.Strings;			use Ada.Strings;
+with Ada.Strings.Fixed;			use Ada.Strings.Fixed;
+with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
+with EarthStation.Platform;		use EarthStation.Platform;
+
+package body EarthStation.Keplerian_Elements is
+
+   Hex_Digits	: constant String := "0123456789abcdef";
+
+   -------------------
+   -- From_Filename --
+   -------------------
+
+   function From_Filename (Input : in String) return String is
+      Char	: Character;
+      Next	: Character;
+      Next_2	: Character;
+      Pos	: Natural := Input'First;
+      Result	: Unbounded_String;
+   begin
+      loop
+         exit when Pos > Input'Length;
+         Char := Input (Pos);
+         if Char /= '%' then
+            Append (Result, Char);
+            Pos := Pos + 1;
+         else
+            Pos := Pos + 1;
+            if Pos > Input'Length then
+               Append (Result, '%');
+               exit;
+            else
+               Next := Input (Pos);
+               if Next = '%' then
+                  Append (Result, '%');
+                  Pos := Pos + 1;
+               else
+                  Pos := Pos + 1;
+                  if Pos > Input'Length then
+                     Append (Result, '%');
+                     Append (Result, Next);
+                     exit;
+                  else
+                     Next_2 := Input (Pos);
+                     Pos := Pos + 1;
+                     Append (Result, Hex_Convert (Next, Next_2));
+                  end if;
+               end if;
+            end if;
+         end if;
+      end loop;
+
+      return To_String (Result);
+   end From_Filename;
+
+   -----------------
+   -- Get_Integer --
+   -----------------
+
+   function Get_Integer (File : access File_Type) return Integer
+   is
+      Temp_Str		: constant String := Get_String (File);
+   begin
+      return Integer'Value (Temp_Str);
+   end Get_Integer;
+
+   --------------------
+   -- Get_Long_Float --
+   --------------------
+
+   function Get_Long_Float (File : access File_Type) return Long_Float
+   is
+      Temp_Str		: constant String := Get_String (File);
+   begin
+      return Long_Float'Value (Temp_Str);
+   end Get_Long_Float;
+
+   ----------------------
+   -- Get_Long_Integer --
+   ----------------------
+
+   function Get_Long_Integer (File : access File_Type) return Long_Integer
+   is
+      Temp_Str		: constant String := Get_String (File);
+   begin
+      return Long_Integer'Value (Temp_Str);
+   end Get_Long_Integer;
+
+   ----------------
+   -- Get_String --
+   ----------------
+
+   function Get_String
+     (File		: access File_Type;
+      Max_Length	: in Positive := 256) return String
+   is
+      Line_Length	: Natural;
+      Temp_String	: String (1 .. Max_Length);
+   begin
+      Get_Line (File.all, Temp_String, Line_Length);
+      return Trim (Temp_String (1 .. Line_Length), Both);
+   end Get_String;
+
+   -----------------
+   -- Hex_Convert --
+   -----------------
+
+   function Hex_Convert
+      (C1		: in Character;
+       C2		: in Character) return String
+   is
+
+      function Hex_Value (Char : in Character) return Natural is
+      begin
+         for i in Hex_Digits'Range loop
+            if Hex_Digits (i) = Char then
+               return i - Hex_Digits'First;
+            end if;
+         end loop;
+         raise CONSTRAINT_ERROR;
+      end Hex_Value;
+
+      CV		: Natural;
+      C1V		: Natural;
+      C2V		: Natural;
+
+   begin
+      C1V := Hex_Value (C1);
+      C2V := Hex_Value (C2);
+      CV := (C1V * 16) + C2V;
+      return "" & Character'Val (CV);
+   exception
+      when CONSTRAINT_ERROR =>
+         return '%' & C1 & C2;
+   end Hex_Convert;
+
+   -----------------------------
+   -- Iterate_Satellite_Names --
+   -----------------------------
+
+   procedure Iterate_Satellite_Names (Iter : in Kep_Iterator) is
+      De	: Directory_Entry_Type;
+      St	: Search_Type;
+   begin
+      Start_Search
+        (Search		=> St,
+         Directory	=> EarthStation.Platform.Get_Keplerian_Elements_Directory,
+         Pattern	=> "*.tle");
+
+      loop
+         exit when not More_Entries (St);
+         Get_Next_Entry
+           (Search		=> St,
+            Directory_Entry	=> De);
+
+         declare
+            Fn	: constant String := From_Filename (Simple_Name (De));
+         begin
+            if Fn /= "." and then Fn /= ".." then
+               exit when not Iter.all (Fn);
+            end if;
+         end;
+      end loop;
+
+      End_Search (St);
+   exception
+      when Ada.IO_Exceptions.NAME_ERROR =>
+         End_Search (St);
+   end Iterate_Satellite_Names;
+
+   ----------
+   -- Load --
+   ----------
+
+   function Load (Name : in String) 
+     return EarthStation.Predict.Keplerian_Elements
+   is
+      Closed	: Boolean := True;
+      File	: aliased File_Type;
+      Filename	: constant String :=
+        EarthStation.Platform.Get_Keplerian_Elements_Directory &
+        To_Filename (Name);
+      Temp	: EarthStation.Predict.Keplerian_Elements;
+   begin
+      Open (File, In_File, Filename);
+      Closed := False;
+
+      declare
+         Name		: constant String := Get_String (File'Access);
+         pragma Unreferenced (Name);
+      begin
+         Temp.Epoch_Year := Get_Integer (File'Access);
+         Temp.Epoch_Time := Get_Long_Float (File'Access);
+         Temp.Inclination := Get_Long_Float (File'Access);
+         Temp.RAAN := Get_Long_Float (File'Access);
+         Temp.Eccentricity := Get_Long_Float (File'Access);
+         Temp.Argument_Perigee := Get_Long_Float (File'Access);
+         Temp.Mean_Anomaly := Get_Long_Float (File'Access);
+         Temp.Mean_Motion := Get_Long_Float (File'Access);
+         Temp.Decay_Rate := Get_Long_Float (File'Access);
+         Temp.Orbit_Number := Get_Long_Integer (File'Access);
+         Temp.ALON := Get_Long_Float (File'Access);
+         Temp.ALAT := Get_Long_Float (File'Access);
+
+         Close (File);
+         return Temp;
+      end;
+   exception
+      when Ada.IO_Exceptions.NAME_ERROR =>
+         raise SATELLITE_NOT_FOUND;
+      when others =>
+         if not Closed then
+            Close (File);
+         end if;
+         raise;
+   end Load;
+
+   ----------
+   -- Save --
+   ----------
+
+   procedure Save
+     (Name		: in String;
+      Elements		: in EarthStation.Predict.Keplerian_Elements)
+   is
+      Closed		: Boolean := True;
+      File		: File_Type;
+      Filename		: constant String :=
+        EarthStation.Platform.Get_Keplerian_Elements_Directory &
+        To_Filename (Name);
+   begin
+      Create (File, Out_File, Filename);
+      Closed := False;
+
+      Put_Line (File, Name);
+      Put_Line (File, Integer'Image (Elements.Epoch_Year));
+      Put_Line (File, Long_Float'Image (Elements.Epoch_Time));
+      Put_Line (File, Long_Float'Image (Elements.Inclination));
+      Put_Line (File, Long_Float'Image (Elements.RAAN));
+      Put_Line (File, Long_Float'Image (Elements.Eccentricity));
+      Put_Line (File, Long_Float'Image (Elements.Argument_Perigee));
+      Put_Line (File, Long_Float'Image (Elements.Mean_Anomaly));
+      Put_Line (File, Long_Float'Image (Elements.Mean_Motion));
+      Put_Line (File, Long_Float'Image (Elements.Decay_Rate));
+      Put_Line (File, Long_Integer'Image (Elements.Orbit_Number));
+      Put_Line (File, Long_Float'Image (Elements.ALON));
+      Put_Line (File, Long_Float'Image (Elements.ALAT));
+
+      Close (File);
+   exception
+      when others =>
+         if not Closed then
+            Close (File);
+         end if;
+         raise;
+   end Save;
+
+   -------------------------------
+   -- TLE_To_Keplerian_Elements --
+   -------------------------------
+
+   function TLE_To_Keplerian_Elements
+     (Line_1	: in String;
+      Line_2	: in String) return EarthStation.Predict.Keplerian_Elements
+   is
+      Temp	: EarthStation.Predict.Keplerian_Elements;
+   begin
+      if Line_1'Length /= 69 or else Line_2'Length /= 69 then
+         raise CONSTRAINT_ERROR;
+      end if;
+
+      declare
+         Epoch_Year_Str	: constant String := "20" & 
+                            Line_1 (Line_1'First + 19 .. Line_1'First + 20);
+         Epoch_Time_Str	: constant String := 
+                            Line_1 (Line_1'First + 21 .. Line_1'First + 32);
+         Inclin_Str	: constant String :=
+                            Line_2 (Line_2'First + 8 .. Line_2'First + 15);
+         RAAN_Str	: constant String :=
+                            Line_2 (Line_2'First + 17 .. Line_2'First + 24);
+         Eccen_Str	: constant String := "0." &
+                            Line_2 (Line_2'First + 26 .. Line_2'First + 32);
+         AP_Str		: constant String := 
+                            Line_2 (Line_2'First + 34 .. Line_2'First + 41);
+         MA_Str		: constant String :=
+                            Line_2 (Line_2'First + 43 .. Line_2'First + 50);
+         MM_Str		: constant String :=
+                            Line_2 (Line_2'First + 52 .. Line_2'First + 62);
+         Orbit_Str	: constant String :=
+                            Line_2 (Line_2'First + 63 .. Line_2'First + 67);
+      begin
+         Temp.Epoch_Year := Integer'Value (Epoch_Year_Str);
+         Temp.Epoch_Time := Long_Float'Value (Epoch_Time_Str);
+         Temp.Inclination := Long_Float'Value (Inclin_Str);
+         Temp.RAAN := Long_Float'Value (RAAN_Str);
+         Temp.Eccentricity := Long_Float'Value (Eccen_Str);
+         Temp.Argument_Perigee := Long_Float'Value (AP_Str);
+         Temp.Mean_Anomaly := Long_Float'Value (MA_Str);
+         Temp.Mean_Motion := Long_Float'Value (MM_Str);
+         Temp.Orbit_Number := Long_Integer'Value (Orbit_Str);
+         Temp.ALON := 0.0;
+         Temp.ALAT := 0.0;
+         return Temp;
+      end;
+   end TLE_To_Keplerian_Elements;
+
+   -----------------
+   -- To_Filename --
+   -----------------
+
+   function To_Filename (Input : in String) return String is
+      Temp	: Unbounded_String;
+   begin
+      if Input'Length = 0 then
+         raise CONSTRAINT_ERROR;
+      end if;
+
+      for i in Input'Range loop
+         case Input (i) is
+            when 'a'..'z' =>
+               Append (Temp, Input (i));
+            when 'A'..'Z' =>
+               Append (Temp, Input (i));
+            when '0'..'9' =>
+               Append (Temp, Input (i));
+            when '%' =>
+               Append (Temp, "%%");
+            when others =>
+               Append (Temp, To_Hex (Input (i)));
+         end case;
+      end loop;
+
+      return To_String (Temp);
+   end To_Filename;
+
+   ------------
+   -- To_Hex --
+   ------------
+
+   function To_Hex (This : in Character) return String is
+      CPos	: constant Integer := Character'Pos (This);
+      C1	: constant Integer := CPos mod 16;
+      C2	: constant Integer := CPos / 16;
+   begin
+      return '%' &
+        Hex_Digits (Hex_Digits'First + C2) &
+        Hex_Digits (Hex_Digits'First + C1);
+   end To_Hex;
+
+end EarthStation.Keplerian_Elements;
+
---
+++ src/earthstation-keplerian_elements.ads
@@ -0,0 +1,93 @@
+--
+--  Copyright (C) 2010 John Vinters
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License along
+--  with this program; if not, see <http://www.gnu.org/licenses/>.
+--
+--  earthstation-keplerian_elements.ads		jvinters	26-May-2010
+--
+
+pragma License (GPL);
+
+with Ada.Text_IO;			use Ada.Text_IO;
+with EarthStation.Predict;
+
+package EarthStation.Keplerian_Elements is
+
+   SATELLITE_NOT_FOUND		: Exception;
+
+   type Kep_Iterator is access function
+     (Satellite_Name		: in String) return Boolean;
+   --  Satellite name iterator.  If the function returns False, then the
+   --  iteration will stop.
+
+   function From_Filename (Input : in String) return String;
+   --  Converts filename to satellite name
+
+   procedure Iterate_Satellite_Names (Iter : in Kep_Iterator);
+   --  Iterates through elements, passing each satellite name to iterator.
+
+   function Load (Name : in String) return EarthStation.Predict.Keplerian_Elements;
+   --  Loads keplerian elements for given satellite name.
+   --  May raise SATELLITE_NOT_FOUND if the given name can't be found.
+
+   procedure Save
+     (Name		: in String;
+      Elements		: in EarthStation.Predict.Keplerian_Elements);
+   --  Saves keplerian elements for given satellite name.
+   --  May raise exceptions if elements can't be saved, or CONSTRAINT_ERROR
+   --  if the name is an empty string.
+
+   function TLE_To_Keplerian_Elements
+     (Line_1	: in String;
+      Line_2	: in String) return EarthStation.Predict.Keplerian_Elements;
+   --  Converts 2 Line Elements to Keplerian Elements.
+   --  May raise CONSTRAINT_ERROR if there is a problem with the TLEs.
+
+   function To_Filename (Input : in String) return String;
+   --  Converts satellite name to filename
+
+private
+
+   function Get_Integer (File : access File_Type) return Integer;
+   --  Reads an Integer from file.  May raise CONSTRAINT_ERROR if the
+   --  line read isn't a valid Integer value.  Other Exceptions may be
+   --  raised if we can't read the file.
+
+   function Get_Long_Float (File : access File_Type) return Long_Float;
+   --  Reads a Long_Float from file.  May raise CONSTRAINT_ERROR if the
+   --  line read isn't a valid Long_Float value.  Other Exceptions may be
+   --  raised if we can't read the file.
+
+   function Get_Long_Integer (File : access File_Type) return Long_Integer;
+   --  Reads a Long_Integer from file.  May raise CONSTRAINT_ERROR if the
+   --  line read isn't a valid Long_Integer value.  Other Exceptions may be
+   --  raised if we can't read the file.
+
+   function Get_String
+     (File		: access File_Type;
+      Max_Length	: in Positive := 256) return String;
+   --  Reads a String from file.  May raise Exceptions if we can't read file.
+   --  Whitespace is trimmed from the beginning and end of the returned string.
+
+   function Hex_Convert
+     (C1		: in Character;
+      C2		: in Character) return String;
+   --  Converts Hex Digits into a single character string (if valid),
+   --  otherwise to string '%' & 'C1' & 'C2'.
+
+   function To_Hex (This : in Character) return String;
+   --  Converts character to '%hh' form where h represents a hex digit
+
+end EarthStation.Keplerian_Elements;
+
